8dc5d702fc54974c5e772278f39482960a7ec35c
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-09-02 18:42:25 +0530

    Update Testing.java

--- day8/src/day8/Testing.java
+++ day8/src/day8/Testing.java
@@ -1,14 +1,33 @@
 package day8;
 
+import java.util.Scanner;
+
 public class Testing {
 	public static void main(String[] args) {
 		Book[] b = new Book[10];
 		Library l = new Library("PPS", "dwarka", b);
+		int n;
+		do {
+		System.out.println("What do you want to do?");
+		System.out.println("1-->Add Book");
+		System.out.println("2-->Remove Book");
+		System.out.println("3-->Show all books");
+		System.out.println("4-->Exit");
 		
-		l.addBook(b);
-		l.showBook(b);
-		l.removeBook(b);
-		l.showBook(b);
+		Scanner s = new Scanner(System.in);
+		
+		n = s.nextInt();
+		
+		switch(n) {
+		case 1 : l.addBook(b);
+			break;
+		case 2 : l.removeBook(b);
+			break;
+		case 3 : l.showBook(b);
+			break;
+		} 
+		s.close();
+	}while(n!=4);
 		
 	}
 
